4c358e647cb53d2f1c2ee5c878f39acbb511e89b
bakhodirsuyarov <83678993+bakhodirsuyarov@users.noreply.github.com>
2021-06-06 16:28:50 +0500

    initial

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// LI va A elementlarini topib olamiz
+var elsFeaturesTabsItem = document.querySelectorAll('.features-tabs__item');
+var elsFeaturesTabsLink = document.querySelectorAll('.features-tabs__link');
+
+// Har bir linkka quloq klikka quloq solishni buyuramiz
+elsFeaturesTabsLink.forEach(function (link) {
+  link.addEventListener('click', function () {
+    // Avval hamma li lardan faollik klassini olib tashlaymiz
+    elsFeaturesTabsItem.forEach(function (item) {
+      item.classList.remove('features-tabs__item--active');
+    });
+
+    // Bosilgan link ota elementiga faollik klassini qo'shamiz
+    link.parentElement.classList.add('features-tabs__item--active');
+  });
+});
+
+
+// FAQ
+var elsQA = document.querySelectorAll('.qa');
+var elsQAToggler = document.querySelectorAll('.qa__toggler');
+
+elsQAToggler.forEach(function (toggler) {
+  toggler.addEventListener('click', function () {
+    elsQA.forEach(function (qa) {
+      qa.classList.remove('qa--active');
+    });
+
+    toggler.closest('.qa').classList.add('qa--active');
+  });
+});
